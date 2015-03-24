88f6b375c0dbceb1d9a00244e66310d6081d813d
Spencer Palmer <palmersp@spencerpalmer.com>
2015-03-24 16:15:48 -0600

    Twitter API

---
+++ TwitterSearch/src/main/java/com/mycompany/twittersearch/SearchTwitter.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.twittersearch;
+
+import java.io.IOException;
+import static java.lang.System.out;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+/**
+ *
+ * @author Spencer
+ */
+@WebServlet(name = "SearchTwitter", urlPatterns = {"/SearchTwitter"})
+public class SearchTwitter extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, TwitterException {
+        
+             Twitter twitter = (Twitter)request.getSession().getAttribute("twitter");
+             out.println(twitter.toString());
+             String searchItem = request.getParameter("term");
+
+        try {
+            Query query = new Query(searchItem);
+
+            QueryResult result = twitter.search(query);
+
+                List<Status> tweets = result.getTweets();
+                String list = "";
+                for (Status tweet : tweets) {
+                   list += "@" + tweet.getUser().getScreenName() + " - " + tweet.getText() + "<br>";
+                }
+
+                request.setAttribute("list", list);
+
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+        request.getRequestDispatcher("showTweets.jsp").forward(request, response);
+
+      
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (TwitterException ex) {
+            Logger.getLogger(SearchTwitter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (TwitterException ex) {
+            Logger.getLogger(SearchTwitter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ TwitterSearch/target/TwitterSearch-1.0-SNAPSHOT/showTweets.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : showTweets
+    Created on : Mar 15, 2015, 6:51:30 PM
+    Author     : Spencer
+--%>
+
+<%@page import="twitter4j.JSONObject"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+      
+        <div>${list}</div>
+        
+
+    </body>
+</html>
